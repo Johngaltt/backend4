c9593fc11db5cad95a27fa6e58ae64478570bc4c
Juan Pablo <chuampa.ros@gmail.com>
2022-10-27 21:09:49 -0300

    desf4

---
+++ store/fakeProducts.js
@@ -0,0 +1,22 @@
+const fakeProducts = [
+  {
+    id: 1,
+    title: 'Monitor GIGABYTE G27Q Gaming',
+    price: 130000,
+    thumbnail: 'https://www.gigabyte.com/FileUpload/Global/KeyFeature/1469/innergigabyteimages/bg1_m.png'
+  },
+  {
+    id: 2,
+    title: 'ROG Crosshair VIII Dark Hero',
+    price: 150000,
+    thumbnail: 'https://www.fullh4rd.com.ar/img/productos/Pics_Prod/mother-asus-x570-rog-crosshair-viii-dark-hero-0.jpg'
+  },
+  {
+    id: 3,
+    title: 'Procesador Amd Ryzen 7 7700x',
+    price: 110000,
+    thumbnail: 'https://http2.mlstatic.com/D_NQ_NP_680879-MLA51754640850_092022-O.jpg'
+  }
+]
+
+module.exports = fakeProducts
